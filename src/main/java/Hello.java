630036d37b215446fd1b1ec7a3c2273e939f8661
Vagaytsev Grigoriy <vagaytsevgrigoriy@gmail.com>
2019-07-22 16:19:20 +0300

    Добавлен метод sayHi() в класс Hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World from Module 1 lesson 4");
+        sayHi();
+    }
+
+    private static void sayHi() {
+        System.out.println("Привет Мир!");
     }
 }
