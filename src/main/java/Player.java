eb89d2352d4fd6d79671897c078c089684c36126
Vagaytsev Grigoriy <vagaytsevgrigoriy@gmail.com>
2019-07-29 16:24:49 +0300

    Добавлены классы Point, Player

---
+++ src/main/java/Player.java
@@ -0,0 +1,8 @@
+/**
+ *  Описывает игрока, который принимает участие в игре ХО
+ */
+public class Player {
+
+    private String name;
+    private String figure;
+}
---
+++ src/main/java/Point.java
@@ -0,0 +1,8 @@
+/**
+ *  Используется для описания точки на плоскости.
+ */
+public class Point {
+
+    private int x;
+    private int y;
+}
