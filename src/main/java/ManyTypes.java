6a1ae9fe8116ace80504a561e254da730fcbe07f
Vagaytsev Grigoriy <vagaytsevgrigoriy@gmail.com>
2019-08-02 11:44:28 +0300

    Добавлен класс ManyTypes

---
+++ src/main/java/ManyTypes.java
@@ -0,0 +1,23 @@
+public class ManyTypes {
+
+    boolean tumbler;
+
+    byte a0 = -128;
+
+    short a1 = 32767;
+
+    Character a2 = '\u0047'; //G
+
+    int a3 = 2147483647;
+
+    long a4 = -9223372036854775808L;
+
+    float a5 = Float.POSITIVE_INFINITY;
+
+    double a6 = Double.NEGATIVE_INFINITY;
+
+    String someName;
+
+    String mail;
+
+}
